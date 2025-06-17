5aa917b42de0b5e6a7b9aa4b41864fc35ab099f3
Andres Breuer <andresbrMymtec@gmail.com>
2025-06-17 17:49:34 -0300

    fix de auditoria

--- src/rag/chat_controller.py
+++ src/rag/chat_controller.py
@@ -7,7 +7,7 @@ from src.rag.vector_store import get_store
 from src.utils.helper_func import format_docs, _format_chat_history
 from src.rag.llm import OpenAiModels
 
-llm: OpenAiModels = OpenAiModels(model_ai='gpt-4.1-mini', temp=0.1)
+llm: OpenAiModels = OpenAiModels(model_ai='gpt-4.1-mini', temp=0)
 
 template = """
 [CONTEXTO]
@@ -21,7 +21,7 @@ Las preguntas estan basadas en un Software de comercio internacional que se enca
 1. NUNCA respondas con información que no esté en el contexto.
 2. SIEMPRE interpretá y explicá la información para que el usuario la entienda claramente.
 3. SI la respuesta incluye pasos, enumeralos de forma clara.
-4. SI hay ambigüedad (por ejemplo, no se especifica si el procedimiento es para exportación o importación), pedí aclaración ANTES de responder.
+4. SI hay ambigüedad (por ejemplo, no se especifica si el procedimiento es para exportación o importación), siempre pedir aclaración del usuario ANTES de responder.
 5. SI NO encontrás la respuesta en el contexto, decí que no la encontrás y pedí reformular la pregunta. NO inventes ni completes con conocimientos propios, y no menciones funtes ni urls.
 6. SIEMPRE que se encuentren documentos al final de la respuesta incluí las fuentes utilizando exactamente este formato:
    - Cada documento citado debe tener su nombre entre los tags <FUENTE></FUENTE>.
@@ -36,6 +36,11 @@ Las preguntas estan basadas en un Software de comercio internacional que se enca
 
 [TU TAREA]
 Leé el contexto, usá solo la información allí contenida, y seguí las reglas anteriores de forma estricta.
+
+[GLOSARIO]
+"impo" = "IMPORTACION"
+"expo" = "EXPORTACION"
+
 
 [Historial de Chat]:
 {chat_history}
